a19ba8e0a7a70888f61163f56e816f8821583c8b
mppatterson84 <mppatterson84@gmail.com>
2019-11-02 19:46:16 -0500

    Update forms.py

--- blog/forms.py
+++ blog/forms.py
@@ -3,14 +3,29 @@ from blog.models import Post
 from django.contrib.auth.models import User
 
 class PostForm(forms.ModelForm):
-    title = forms.CharField(label='', required=True, widget=forms.TextInput(
-            attrs={"placeholder": "Post Title", "class": "form-control"}))
+    title = forms.CharField(
+        label='', 
+        required=True, 
+        widget=forms.TextInput(
+            attrs={
+                "placeholder": "Post Title", 
+                "class": "form-control"
+            }
+        )
+    )
 
-    body = forms.CharField(label='', required=True, widget=forms.Textarea(
-           attrs={"placeholder": "Post Body Content", 
-                  "class": "form-control", 
-                  "id": "bodyTextArea", 
-                  "rows": "15"}))
+    body = forms.CharField(
+        label='', 
+        required=True, 
+        widget=forms.Textarea(
+            attrs={
+                "placeholder": "Post Body Content", 
+                "class": "form-control", 
+                "id": "bodyTextArea", 
+                "rows": "15"
+            }
+        )
+    )
     
     class Meta:
         model = Post
