5e352302eb72703403ad57890fff46c840523d6e
mppatterson84 <mppatterson84@gmail.com>
2019-11-02 22:35:17 -0500

    Update settings.py

--- blog_project/settings.py
+++ blog_project/settings.py
@@ -133,11 +133,15 @@ STATIC_URL = '/static/'
 
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
-STATICFILES_DIRS = [os.path.join(BASE_DIR, 'staticfiles')]
-
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
 CKEDITOR_BASEPATH = "/static/ckeditor/ckeditor"
+
+STATICFILES_FINDERS = (
+    'django.contrib.staticfiles.finders.FileSystemFinder',
+    'django.contrib.staticfiles.finders.AppDirectoriesFinder',
+    'django.contrib.staticfiles.finders.DefaultStorageFinder',
+ )
 
 # Activate Django-Heroku.
 django_heroku.settings(locals())
