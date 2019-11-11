60783cbacba54048ed5d15ebef25051cbb2e7c57
mppatterson84 <mppatterson84@gmail.com>
2019-11-11 11:55:54 -0600

    Update settings.py

--- blog_project/settings.py
+++ blog_project/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = os.environ['SECRET_KEY']
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = os.environ['DEBUG']
 
-ALLOWED_HOSTS = ['localhost', '127.0.0.1']
+ALLOWED_HOSTS = ['localhost', '127.0.0.1', 'https://stormy-ocean-61170.herokuapp.com']
 
 
 # Application definition
